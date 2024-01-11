cbd9988204b5a0efb5de765db681ea762d554753
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-01-11 21:43:27 +0200

    return render page

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -6,10 +6,21 @@ class Main extends React.Component {
     movies: [],
   };
 
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=21eb5ef&s=matrix")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <main className="container content">
-        <Movies movies={this.state.movies} />
+        {movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : (
+          <h3>Loading...</h3>
+        )}
       </main>
     );
   }
