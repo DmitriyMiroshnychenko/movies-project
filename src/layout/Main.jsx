a7be3bcd3fdbb6c74f2cb95c89dce54b3585cad4
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-01-11 21:30:50 +0200

    changed files

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,18 @@
-function Main() {
-  return <main className="container content">Hello from Movies App</main>;
+import React from "react";
+import { Movies } from "../components/Movies";
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  render() {
+    return (
+      <main className="container content">
+        <Movies movies={this.state.movies} />
+      </main>
+    );
+  }
 }
 
 export { Main };
